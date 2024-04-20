ef7853761c35cf983c9af287fc73f1b99c59f26b
b22robhe <96245488+b22robhe@users.noreply.github.com>
2024-04-20 23:37:08 +0200

    Gradle and Button code fixed gradle added onClick startActivity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.screens;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class); // change this line and you will good to go
+                    startActivity(intent);
+            }
+        });
+
     }
 }
