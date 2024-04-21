d36a3e57a680e9fa75bdcf28f0c381ea442392c2
b22robhe <96245488+b22robhe@users.noreply.github.com>
2024-04-21 19:03:35 +0200

    EditText and ErrorTextView added an editText to take an input and send it to the 2nd activity added an error text to tell the user to fill in the editText

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +20,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button btn = findViewById(R.id.button);
-
+        final EditText input = findViewById(R.id.reverseInput);
+        final TextView errorText = findViewById(R.id.errorText);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if (input == null) {
+                    errorText.setText("Please enter text to reverse");
+                }
                     Intent intent = new Intent(MainActivity.this, MainActivity2.class); // change this line and you will good to go
                     startActivity(intent);
+
+
             }
         });
 
